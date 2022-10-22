3ff549e64a38887c7481d46ccf219887de79f197
brittany <brittanyfreitas9@gmail.com>
2022-10-21 22:41:23 -0400

    styling

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 
 //setting my varibles to be able to target the menu icon and the exit icon
-const menuButton = document.querySelector('.menuIcon')
-const closeNav = document.querySelector('.exit');
-const slideMenu = document.querySelector('.slideOutNav')
+const menuButton = document.querySelector('.menuIcon');
+const closeNav = document.querySelector('.exit', ".scroll");
+const slideMenu = document.querySelector('.slideOutNav');
 
 //attaching an event listener to my menu so that on click it adds the class of .open
-menuButton.addEventListener ("click", function(){
+menuButton.addEventListener ("click", () => {
     slideMenu.classList.add('open');
 });
 
 //attaching an event listener to my exit icon such that when clicked it removes the class of .open
-closeNav.addEventListener ("click", function(){
+closeNav.addEventListener ("click", () => {
     slideMenu.classList.remove('open');
+});
+
+//attaching event listener to buttons to scroll parts of page into view
+const checkOutWork = document.querySelector('.checkOutWork')
+checkOutWork.addEventListener("click", () => {
+    const myWork = document.querySelector('.myWork')
+    myWork.scrollIntoView({behavior: "smooth"})
+});
+
+const getInTouch = document.querySelector('.getInTouch')
+getInTouch.addEventListener("click", () => {
+    const contact = document.querySelector('.contact')
+    contact.scrollIntoView({behavior: "smooth"})
 });
